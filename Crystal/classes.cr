4bdb267042bf2bfeb1aff0b042e439f15437a2cd
gavr <gavr123456789@gmail.com>
2021-11-10 04:23:34 +0300

    first commit

---
+++ Crystal/classes.cr
@@ -0,0 +1,19 @@
+class Person
+  @age = 0
+
+  def initialize(@name : String)
+  end
+
+  def age
+    @age
+  end
+
+  def become_older
+    @age += 1
+  end
+end
+
+john = Person.new "john"
+
+john.become_older
+puts john.age
---
+++ Crystal/tagged_union.cr
@@ -0,0 +1,10 @@
+t1 = {x: 1, y: "hi"}   # Tuple(x: Int32, y: String)
+t2 = {y: true, x: nil} # Tuple(y: Bool, x: Nil)
+
+t3 = rand < 0.5 ? t1 : t2
+typeof(t3) # NamedTuple(x: Int32 | Nil, y: String | Bool)
+
+
+record StageInitial, data : String
+record StageFinished, data : String
+alias Stage = StageInitial | StageFinished
