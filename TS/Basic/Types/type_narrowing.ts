db7980a1eac995094cb0f8d28e47014c89eb5339
gavr <gavr123456789@gmail.com>
2021-12-10 01:59:14 +0300

    type narrowing ts

---
+++ TS/Basic/Types/type_narrowing.ts
@@ -0,0 +1,31 @@
+
+type NotNullableObject = {
+  a: number
+}
+
+function p(x: NotNullableObject) {
+  console.log(x.a)
+}
+
+p(null)
+
+
+type NullableObject = NotNullableObject | null
+
+let x: NullableObject = null
+
+if (x === null) {
+  p(x)
+} else {
+  p(x) // Narrowed
+}
+
+type NumOrStr = number | string
+
+function foo(x: NumOrStr) {
+  if (typeof x === "number") {
+    return x / 5 // Narrowed
+  }
+  return x / 5
+}
+
