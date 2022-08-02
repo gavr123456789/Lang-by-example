34a1ef38c08d4295b889007260569cfd740b41e4
gavr <gavr123456789@gmail.com>
2022-08-02 03:41:49 +0300

    add parcer nim example

---
+++ Nim/old/niminterpret/main.nim
@@ -0,0 +1,81 @@
+import std/strutils
+import std/parseutils
+{.experimental: "strictNotNil".}
+
+type 
+  TokenType = enum 
+    INTEGER, PLUS, EOF
+  Token = object 
+    case kind: TokenType
+    of INTEGER: value: int
+    of PLUS: discard
+    of EOF: discard
+
+func toString(self: Token): string =
+  "Tonen(" & $self.kind & ", " & $self.value & ")"
+  
+type Interpreter = object 
+  text: string
+  pos: int
+  current_token: Token
+proc error(self: Interpreter)  =
+  raise newException(OSError, "parsing error")
+
+proc get_next_token(self: var Interpreter): Token =
+  var text = self.text
+
+  if self.pos > (len(text) - 1):
+    return Token(kind: EOF)
+
+  var current_char = text[self.pos]
+  
+  if current_char.isDigit:
+    self.pos += 1
+    return Token(kind: INTEGER, value: ($current_char).parseInt)
+
+  if current_char == '+':
+    self.pos += 1
+    return Token(kind: PLUS)
+
+  self.error
+  return Token(kind: EOF)
+
+var eatCounter = 0
+proc eat(self: var Interpreter, tokenKind: TokenType): void =
+  eatCounter += 1
+  if self.current_token.kind == tokenKind:
+    self.current_token = self.get_next_token
+      
+
+  else:
+    echo "error: ", self.current_token, ", ", tokenKind
+    self.error
+
+proc expr(self: var Interpreter): int =
+  self.current_token = self.get_next_token
+
+  let left = self.current_token
+  self.eat INTEGER
+
+  # let op = self.current_token
+  self.eat PLUS
+
+  let right = self.current_token
+  self.eat INTEGER
+
+  echo "left = ", left
+  echo "r = ", right
+
+  result = left.value + right.value
+
+proc main() =
+  while true:
+    let text = stdin.readLine
+    if text == "q":
+      break;
+    
+    var interpreter = Interpreter(text: text)
+    let res = interpreter.expr
+    echo "res = ",  res
+
+main()
